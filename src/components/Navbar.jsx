5215f330397d47eb1d956095e62b7ec9b0f64c43
Dylan Simon <grvdylans@gmail.com>
2025-10-31 15:02:01 -0600

    Started using modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router";
+import styles from "./Navbar.module.css"
 
 function Navbar() {
     return (
-        <div className="nav-bar">
+        <div className={styles.navBar}>
             <Link to="/">Homepage</Link>
             <Link to="shop">Shop</Link>
             <Link to="cart/">Cart</Link>
