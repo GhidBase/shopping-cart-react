ba4b7c17c5195bff2e48987a0765518c03ca8fc9
Dylan Simon <grvdylans@gmail.com>
2025-10-31 13:05:47 -0600

    Added initial testing

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,6 +1,9 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "../App.jsx";
+import { MemoryRouter } from "react-router";
+import "@testing-library/jest-dom";
+import Shop from "../components/Shop.jsx";
 
 describe("something truthy and falsy", () => {
     it("true to be true", () => {
@@ -12,12 +15,21 @@ describe("something truthy and falsy", () => {
     });
 });
 
-describe("App", () => {
-    it("renders headline", () => {
-        render(<App title="React" />);
-
-        screen.debug();
-
-        // check if App components renders headline
+describe("Webpages", () => {
+    it("Homepage shows menu options", () => {
+        render(
+            <MemoryRouter initialEntries={["/"]}>
+                <App title="React" />
+            </MemoryRouter>
+        );
+        expect(screen.getByText("Go to shop")).toBeInTheDocument();
+    });
+    it('Shop shows "Back to main"', () => {
+        render(
+            <MemoryRouter initialEntries={["/"]}>
+                <Shop />
+            </MemoryRouter>
+        );
+        expect(screen.getByText("Back to main")).toBeInTheDocument();
     });
 });
